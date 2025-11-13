096d295888fe8b0e4e5e736d2c6fd81ca8b25c12
Rostyslav Rava <rostyslav@ravasoft.com>
2025-11-13 12:01:43 -0800

    Add project structure

---
+++ src/main/java/io/perfiora/App.java
@@ -0,0 +1,13 @@
+package io.perfiora;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
