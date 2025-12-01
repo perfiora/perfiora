c86a016b6a6c1f4eaa8f388454f1265e57baa269
Rostyslav Rava <rostyslav@ravasoft.com>
2025-12-01 13:03:24 -0800

    Define getters and setters

--- src/main/java/io/perfiora/virtual/VirtualColumn.java
+++ src/main/java/io/perfiora/virtual/VirtualColumn.java
@@ -12,4 +12,36 @@ public class VirtualColumn extends VirtualObject {
     private boolean isNullable;
 
     private Integer length;
+
+    public VirtualDataType getDataType() {
+        return dataType;
+    }
+
+    public void setDataType(VirtualDataType dataType) {
+        this.dataType = dataType;
+    }
+
+    public VirtualIndex getIndex() {
+        return index;
+    }
+
+    public void setIndex(VirtualIndex index) {
+        this.index = index;
+    }
+
+    public boolean isNullable() {
+        return isNullable;
+    }
+
+    public void setNullable(boolean nullable) {
+        isNullable = nullable;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
 }
